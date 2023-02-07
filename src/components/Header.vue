f183c9cf437c9174de241458c3b864c05be3cb4d
J-b-benetti <j.b.benetti81@gmail.com>
2023-02-07 20:35:41 +0100

    Commit du Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>L'Echapée Livre</h1>
+
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Lobster+Two&family=Roboto:wght@100&family=Source+Code+Pro:wght@200&display=swap');
+
+h1 {
+    color: #2ba14b;
+    font-family: 'Lobster Two', cursive;
+    font-size: 60px;
+    text-shadow: 2px 2px #88a3cf;
+    text-align: center;
+    text-decoration: #3a2885 wavy underline;
+}
+</style>
